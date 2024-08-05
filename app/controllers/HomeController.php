dca004fcfb256fe570aa2c74dbe6e67faff2e419
Costin Ionuț <51002637+staark-dev@users.noreply.github.com>
2024-08-05 18:53:35 +0200

    Fix pula

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
-class Home extends Controller {
-    
+class HomeController extends Controller {
+
     public function index() {
         //var_dump($this->query("SELECT * FROM `accounts` ORDER BY uID DESC LIMIT 0, 10"));
         $this->view('welcome');
--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -1,7 +1,5 @@
 <?php
 class UsersController extends Controller {
-    use Users;
-
     public function index() {
         var_dump($this->modelName);
         $this->view('users', []); //$this->model->get()
